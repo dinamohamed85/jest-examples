45615637a1b427aa976fbeb83a84857579d5141d
dinamohamed85 <dina.elghareeb@gmail.com>
2022-10-31 23:23:23 +0100

    object toHaveProperty method added

--- README.md
+++ README.md
@@ -7,6 +7,6 @@
 - only and skip method 
 - beforeAll and beforeEach
 - afterAll and afterEach
-- Matchers methods : toHavelength , toContain , not.toContain , not.toBe
+- Matchers methods : toHavelength , toContain , not.toContain , not.toBe , isNaN , toBeFalsy , toBeTrusy
 - Matchers methods : toBeGreaterThan , toBeGreaterThanOrEqual , toBeLessThan , toBeLessThanOrEqual 
-- Matchers methods : toBeUndefined , toMatch
+- Matchers methods : toBeUndefined , toMatch , toHaveProperty
--- example5/matchers.test.js
+++ example5/matchers.test.js
@@ -17,43 +17,37 @@ test('check if array not contains number 7', () => {
 });
 
 test('check if array not contains number zero', () => {
-    for(let i=0; i<alldata.length;i++)
-    {
+    for (let i = 0; i < alldata.length; i++) {
         expect(alldata[i]).not.toBe(0);
     }
 });
 
 test('check if array contains only numbers 1st method', () => {
-    for(let i=0; i<alldata.length;i++)
-    {
+    for (let i = 0; i < alldata.length; i++) {
         expect(isNaN(alldata[i])).toBe(false);
     }
 });
 
 test('check if array contains only numbers 2nd method', () => {
-    for(let i=0; i<alldata.length;i++)
-    {
+    for (let i = 0; i < alldata.length; i++) {
         expect(isNaN(alldata[i])).toBeFalsy;
     }
 });
 
 test('check if array first element is greater than 1', () => {
-    for(let i=0; i<alldata.length;i++)
-    {
+    for (let i = 0; i < alldata.length; i++) {
         expect(alldata[0]).toBeGreaterThan(1);
     }
 });
 
 test('check if array first element is greater than or equal 1', () => {
-    for(let i=0; i<alldata.length;i++)
-    {
+    for (let i = 0; i < alldata.length; i++) {
         expect(alldata[0]).toBeGreaterThanOrEqual(1);
     }
 });
 
 test('check if array first element is less than 5', () => {
-    for(let i=0; i<alldata.length;i++)
-    {
+    for (let i = 0; i < alldata.length; i++) {
         expect(alldata[0]).toBeLessThan(5);
     }
 });
@@ -61,10 +55,25 @@ test('check if array first element is less than 5', () => {
 test('check Undefined variable', () => {
     let a;
     expect(a).toBeUndefined();
- });
- 
- test('check if substring inside string by RegExp', () => {
-    let str="I love my children Malika & Zahra";
+});
+
+test('check if substring inside string by RegExp', () => {
+    let str = "I love my children Malika & Zahra";
     expect(str).toMatch(/Malika/);
- });
- 
+});
+
+test('check if object has this property', () => {
+    let obj = {
+        name: "dina",
+        age: "37"
+    };
+    expect(obj).toHaveProperty("age");
+});
+
+test('check if object has this property and specific value', () => {
+    let obj = {
+        name: "dina",
+        age: 37
+    };
+    expect(obj).toHaveProperty("age",37);
+});
