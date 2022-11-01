49e5636387cc81595d78fa498b8f6f9363fb8bfb
dinamohamed85 <dina.elghareeb@gmail.com>
2022-11-01 14:41:10 +0100

    test , expect Methods , string methods

--- example2/input.test.js
+++ example2/input.test.js
@@ -4,14 +4,14 @@ test('Check If no name is given it will show UnKnown', () => {
     expect(theFilter()).toBe("UnKnown");
 })
 
-test('Check name starts or ends with space the name will be trimmed', () => {
+test('Check name starts or ends with space, the name will be trimmed', () => {
     expect(theFilter(" dina ")).toBe("dina");
 })
 
-test('Check if lentgh > 10 name will be showd only first 10 charachters', () => {
+test('Check if lentgh > 10, name will be showd only first 10 charachters', () => {
     expect(theFilter("dina_mohamed_ahmed")).toBe("dina_moham");
 })
 
-test('Check if name starts with nderscore name will not show underscore', () => {
+test('Check if name starts with underscore, name will not show underscore', () => {
     expect(theFilter("_dina")).toBe("dina");
 })
